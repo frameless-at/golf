84b215d4742631771c5cb67ffca5fabe617c2484
frameless Media <office@frameless.at>
2025-04-29 12:35:25 +0200

    Update contactform.config.php
    added testmail-adress

--- contactform/inc/contactform.config.php
+++ contactform/inc/contactform.config.php
@@ -1,5 +1,12 @@
 <?php
-$cfg['email'] = 'sl.golf@hsv-wien.at';
+$host = str_replace('www.', '', $_SERVER['HTTP_HOST']);
+
+if ($host !== 'golf.frameless.at') {
+	$cfg['email'] = 'm.kozak@hframeless.at';
+} else {
+	$cfg['email'] = 'sl.golf@hsv-wien.at';
+}
+
 $cfg['form_validationmessage'] = 'Die Generierung Ihres Mitgliedsantrags war erfolgreich. Sie erhalten umgehend Ihr Anmeldeformular per E-Mail.<br /><br />Schönes Spiel, Ihr HSV wiengolf';
 $cfg['form_error_captcha'] = 'Wert stimmt nicht &uuml;berein';
 $cfg['form_error_emptyfield'] = 'Dieses Feld muss ausgefüllt werden.';
